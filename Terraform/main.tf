2d2fc1b22407b13e06e9e0ae0a06be7e27144be4
Dipak Yadav <Dipak.Yadav@neudesic.com>
2021-01-19 15:17:49 +0530

    added terraform files

---
+++ Terraform/main.tf
@@ -0,0 +1,99 @@
+provider "azurerm" {
+
+features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource-group-name}"
+  location = "${var.location}"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "example-appserviceplan"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app-service" {
+  name                = "${var.app-service-name}"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.plan.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_sql_server.sqldb.fully_qualified_domain_name} Database=${azurerm_sql_database.db.name};User ID=${azurerm_sql_server.sqldb.administrator_login};Password=${azurerm_sql_server.sqldb.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+  }
+}
+
+resource "azurerm_sql_server" "sqldb" {
+  name                         = "terraform-sqlserver1"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  location                     = "${azurerm_resource_group.rg.location}"
+  version                      = "12.0"
+  administrator_login          = "dipak"
+  administrator_login_password = "Dk9014094502@"
+}
+
+resource "azurerm_sql_database" "db" {
+  name                = "terraform-sqldatabase"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rg.location}"
+  server_name         = "${azurerm_sql_server.sqldb.name}"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "terraform-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
